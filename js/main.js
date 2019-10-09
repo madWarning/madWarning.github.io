d3acbbdb0f27bea64afe6e24a38689157711c443
madWarning <mad_warning@hotmail.com>
2019-10-09 18:20:54 +0200

    Primera versión

---
+++ js/main.js
@@ -0,0 +1,42 @@
+//se ejecuta al estar el docuemnto/DOM cargado 
+$(document).ready(function(){
+    
+    //cambio estilo barra nav
+    
+    //on: detector de eventos
+    $(window).on("scroll", function(){
+        var posicionParrafo = $("#primera").offset();
+        var posicionVentana = $(window).scrollTop();
+        var logoNegro = document.getElementById("logoNegro");
+        var logoBlanco = document.getElementById("logoBlanco");
+        //console.log(position.top);
+        if(posicionVentana > posicionParrafo.top - 90){
+            $("nav").removeClass("white");
+            $(".burger").removeClass("white");
+            console.log("mostramos barra");
+            logoNegro.style.display="block";
+            logoBlanco.style.display="none";
+            // $("#logoNegro").style.display("block");
+            // $("#logoBlanco").style.display="none";
+            
+        } else {
+            $("nav").addClass("white");
+            $(".burger").addClass("white");
+            console.log("ocultamos");
+            logoBlanco.style.display="block";
+            logoNegro.style.display="none";
+            // $("#logoNegro").style.display="none";
+            // $("#logoBlanco").style.display="block";
+        }
+    });
+
+    
+    /*** menú lateral */
+    // punto identifica la clase
+    $(".burger").click(function(){
+        $(".burger").toggleClass("open");
+        $("aside").toggleClass("open");
+    });
+
+
+});
